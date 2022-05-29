60cafa32ce11cf7cfd7edd0cc199adb82b47b1fc
Everton Santos <everton_santos_01@hotmail.com>
2022-05-29 05:16:14 -0300

    Arquivo sql
    Arquivo sql para consultar os comandos usando no banco de dados MariaDB

---
+++ Fornecedor/Consulta.sql
@@ -0,0 +1,7 @@
+create database fornecedorbd;
+
+use fornecedorbd;
+
+create table fornecedor(id int, nome char(100), data date);
+
+select * from fornecedor;
---
+++ Loja/Consulta.sql
@@ -0,0 +1,7 @@
+create database lojabd;
+
+use lojabd;
+
+create table loja(id int, nome char(100), data date);
+
+select * from loja;
